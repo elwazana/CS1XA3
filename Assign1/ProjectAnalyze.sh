d1921278ff2823790bbd5b27590df8c38f772186
Akram Elwazani <elwazana@mills.cas.mcmaster.ca>
2018-02-14 23:48:29 -0500

    Debugged and began implementing new feature

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 read Input
-if [ "$Input" = state ]
-then
+if [ "$Input" = "state" ]; then
 	git status
 
-elif [ "$Input" = changes ]
-	git diff > changes.log
+elif [ "$Input" = "changes" ]; then
+	git diff >> changes.log
+
+elif [ "$Input" = "todo" ]; then
+	grep -r --exclude=todo.log "#TODO" ~/CS1XA3/Assign1 >> todo.log
+
+elif [ "$Input" = "due" ]; then
+	echo $(date "+%Y %b %d This is a placeholder")  
 
 else
 	echo "No such command is avaliable"
