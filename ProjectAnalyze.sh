2560ecca7d730648059e1b2481055bece81410a4
Akram Elwazani <elwazana@mills.cas.mcmaster.ca>
2018-02-20 09:11:14 -0500

    Added copy of ProjectAnalyze.sh into root repo CS1XA3

---
+++ ProjectAnalyze.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+Input=filler
+while [ "${Input,,}" != "exit" ]
+do
+	read -p "Enter command: "  Input
+	if [ "${Input,,}" = "state" ]; then
+		git status
+		
+		read -p "Would you like to attempt an update? (Y/N): " Input2
+		if [ "${Input2,,}" = "y" ]; then
+		
+			git pull
+
+		fi
+		
+	elif [ "${Input,,}" = "changes" ]; then
+		git diff > changes.log
+		
+		read -p "Would you like to view changes.log? (Y/N): " Input2
+		if [ "${Input2,,}" = "y" ]; then
+		
+			vim changes.log
+		
+		fi
+
+	elif [ "${Input,,}" = "todo" ]; then
+		grep -r --exclude=todo.log "#TODO" ~/CS1XA3/Assign1 > todo.log
+
+		read -p "Would you like to view todo.log? (Y/N): " Input2
+		if [ "${Input2,,}" = "y" ]; then
+
+			vim todo.log
+
+		fi
+
+	elif [ "${Input,,}" = "error" ];then
+		ghc -fno-code *.hs > error.log
+
+		read -p "Would you like to view error.log? (Y/N): " Input2
+		if [ "${Input2,,}" = "y" ]; then
+
+			vim error.log
+
+		fi
+
+	elif [ "${Input,,}" = "due1" ]; then
+		./Due1.sh
+
+	elif [ "${Input,,}" = "due2" ]; then
+		./Due2.sh
+
+	elif [ "${Input,,}" = "due3" ]; then
+                ./Due3.sh
+	
+	elif [ "${Input,,}" = "exit" ]; then
+		break
+	
+	else
+		echo "No such command exists" 
+	fi
+	
+done
