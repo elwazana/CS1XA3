df34bf754f27f298481ad3c1429a120a1c536d8c
Akram Elwazani <elwazana@mills.cas.mcmaster.ca>
2018-02-26 16:03:51 -0500

    Minor edits

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -1,78 +1,74 @@
 #!/bin/bash
 
-Input=filler
-while [ "${Input,,}" != "exit" ]
+# Main script of ProjectAnalyze
+INPUT=	# Blank input needed for looping execution
+while [ "${INPUT,,}" != "exit" ] # Loop execution condition
 do
-	read -p "Enter command: "  Input
-	if [ "${Input,,}" = "state" ]; then
+	read -p "Enter command: "  INPUT	# Main user command input
+	
+	if [ "${INPUT,,}" = "state" ]; then	# state command: checks if local repo and master repo are the same
 		git status
 		
-		read -p "Would you like to attempt an update? (Y/N): " Input2
-		if [ "${Input2,,}" = "y" ]; then
+		read -p "Would you like to attempt an update? (Y/N): " INPUT2	# Attempt update, asks to see if the user wants to try an update after executing the repo check feature
+
+		if [ "${INPUT2,,}" = "y" ]; then
 		
 			git pull
 
 		fi
 		
-	elif [ "${Input,,}" = "changes" ]; then
+	elif [ "${INPUT,,}" = "changes" ]; then	# changes command: creates a file with all the uncommitted changes
 		git diff > changes.log
 		
-		read -p "Would you like to view changes.log? (Y/N): " Input2
-		if [ "${Input2,,}" = "y" ]; then
+		read -p "Would you like to view contents of changes.log? (Y/N): " INPUT2	# Viewing created file, asks if the user wants to view the created file
+		if [ "${INPUT2,,}" = "y" ]; then
 		
-			vim changes.log
+			cat changes.log
 		
 		fi
 
-	elif [ "${Input,,}" = "todo" ]; then
-		grep -r -h --exclude=ProjectAnalyze.sh --exclude=*.log --exclude-dir=Assign1/Features "#TODO" ~/CS1XA3/ > todo.log
+	elif [ "${INPUT,,}" = "todo" ]; then	# todo command: 
+		grep -r -h  --exclude=ProjectAnalyze.sh --exclude=Assign1/ProjectAnalyze.sh/ --exclude=todo.log "#TODO" ~/CS1XA3/ > todo.log
 
-		read -p "Would you like to view todo.log? (Y/N): " Input2
-		if [ "${Input2,,}" = "y" ]; then
+		read -p "Would you like to view contents of todo.log? (Y/N): " INPUT2	# Viewing created file, asks if the user wants to view the created file
+		if [ "${INPUT2,,}" = "y" ]; then
 
-			vim todo.log
+			cat todo.log
 
 		fi
 
-	elif [ "${Input,,}" = "error" ];then
-		find . -name *.hs -exec ghc -fno-code {} \; &> error.log
+	elif [ "${INPUT,,}" = "error" ];then	# error command: creates a file 'error.log' with all syntax errors found in all haskell files
+		find ~/CS1XA3/ -type f -name "*.hs" -exec ghc -fno-code {} \; &> error.log
 		
-		if [[ -s error.log ]]; then
-			read -p "Would you like to view error.log? (Y/N): " Input2
-			if [ "${Input2,,}" = "y" ]; then
-
-				vim error.log
-
+		if [[ -s error.log ]]; then	# Checks if there are any errors
+			read -p "Would you like to view contents of error.log? (Y/N): " INPUT2	# Viewing created file, asks if the user wants to view the created file
+			if [ "${INPUT2,,}" = "y" ]; then
+				cat error.log
 			fi
 		else
 			echo "No errors were found"
-
 		fi
 
-	elif [ "${Input,,}" = "due1" ]; then
-		./Assign1/Features/Due1.sh
-
-	elif [ "${Input,,}" = "due2" ]; then
-		./Assign1/Features/Due2.sh
-
-	elif [ "${Input,,}" = "due3" ]; then
-                ./Assign1/Features/Due3.sh
+	# Additional feature executed as sub-scripts all stored at CS1XA3/Assign1/Features
+	elif [ "${INPUT,,}" = "due" ]; then	
+		./Assign1/Features/Due.sh	
 	
-	elif [ "${Input,,}" = "time" ]; then
+	elif [ "${INPUT,,}" = "time" ]; then
 		./Assign1/Features/Time.sh
 
-	elif [ "${Input,,}" = "search" ]; then
+	elif [ "${INPUT,,}" = "search" ]; then
 		./Assign1//Features/Search.sh
 
-	elif [ "${Input,,}" = "replace" ]; then
+	elif [ "${INPUT,,}" = "replace" ]; then
 		./Assign1/Features/Replace.sh
 	
-	elif [ "${Input,,}" = "diff" ]; then
+	elif [ "${INPUT,,}" = "diff" ]; then
 		./Assign1/Features/Diff.sh	
 
-	elif [ "${Input,,}" = "exit" ]; then
+	elif [ "${INPUT,,}" = "exit" ]; then
 		break	
 
+	# Error output if the command user input is non-existent
 	else
 		echo "No such command exists" 
 	fi
