b382eddc2809f502f1000dfdc4acccb2c41b6600
Akram Elwazani <elwazana@mills.cas.mcmaster.ca>
2018-02-26 15:10:13 -0500

    Debugging Diff.sh

--- Assign1/Features/Diff.sh
+++ Assign1/Features/Diff.sh
@@ -3,16 +3,15 @@
 # Obtained from Jessica de Leeuw (ie deleeuwj), modified with aid from [ cite: https://stackoverflow.com/questions/9964823/how-to-check-if-a-file-is-empty-in-bash ]
 
 # diff command: displays creates and possibly displays the differences between the local repo and the master repo
-read -p "Would you like to see the differences between your Local and Remote Repos? (Y/N) " ANS
-if [ "${ANS,,}" = "y" ]; then
-	D=$"git diff origin/master master"	# Finds the differences then stores them in a file diffs.log
-        $D &> diffs.log
+D=$"git diff origin/master master"	# Finds the differences then stores them in a file diffs.log
+$D &> diffs.log
 
-	if  [[ -s diffs.log ]]; then # (Modification) Checking if there are differences between the repos
+if  [[ -s diffs.log ]]; then # (Modification) Checking if there are differences between the repos
+	read -p "Would you like to view the content of diff.log? (Y/M): " ANS
+	if [ "${ANS,,}" = "y" ]; then
 		cat diffs.log
-	
-	# If there are no differences then output error message
-	else
-		echo "There are no differences between the local and master repositories"
 	fi
+	# If there are no differences then output error message
+else
+	echo "There are no differences between the local and master repositories"
 fi
